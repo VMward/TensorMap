63e1865e5a7bd50ce9be5c1f6618a4e80570cdcb
benjaminfan <ben.yan.fan@gmail.com>
2020-11-11 19:54:31 -0600

    Creating decoder for geohash

--- TensorMap/geohash/geohash.py
+++ TensorMap/geohash/geohash.py
@@ -482,6 +482,19 @@ for i in range(len(__base32)):
     __decodemap[__base32[i]] = i
 del i
 
+def decode(geohash):
+    """
+    Decode geohash, returning two strings with latitude and longitude
+    containing only relevant digits and with trailing zeroes removed.
+    """
+    lat, lon, lat_err, lon_err = decode_exactly(geohash)
+    # Format to the number of decimals that are known
+    lats = "%.*f" % (max(1, int(round(-log10(lat_err)))) - 1, lat)
+    lons = "%.*f" % (max(1, int(round(-log10(lon_err)))) - 1, lon)
+    if '.' in lats: lats = lats.rstrip('0')
+    if '.' in lons: lons = lons.rstrip('0')
+    return lats, lons
+
 def encode(latitude, longitude, precision=12):
     """
     Encode a position given in float arguments latitude, longitude to
